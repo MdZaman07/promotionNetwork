41d7136d0afe53668b91b2c566c171643195d443
Toan Nguyen <toan@s1t2.com>
2023-05-15 15:23:11 +1000

    add remaining views, add view controllers for all views

---
+++ ios-ass3-promotion-network/Controller/CreateProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ios-ass3-promotion-network
+//
+//  Created by Malena Diaz Rio on 2/5/23.
+//
+
+import UIKit
+
+class CreateProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
