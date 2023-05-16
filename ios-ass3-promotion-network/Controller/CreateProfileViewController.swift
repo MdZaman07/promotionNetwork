a59712ba04f07678e31ea798ed6125397a1bcbb3
Toan Nguyen <toan@s1t2.com>
2023-05-16 17:18:37 +1000

    create user with dummy data

--- ios-ass3-promotion-network/Controller/CreateProfileViewController.swift
+++ ios-ass3-promotion-network/Controller/CreateProfileViewController.swift
@@ -9,9 +9,40 @@ import UIKit
 
 class CreateProfileViewController: UIViewController {
 
+    @IBOutlet weak var createButton: UIButton!
+    @IBOutlet weak var descriptionField: UITextField!
+    @IBOutlet weak var confirmPasswordField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var cityField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var lastNameField: UITextField!
+    @IBOutlet weak var firstNameField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func createProfile(_ sender: Any) {
+        // Unwrap optionals
+        guard let username = usernameField.text else {return}
+        guard let firstName = firstNameField.text else {return}
+        guard let lastName = lastNameField.text else {return}
+        guard let password = passwordField.text else {return}
+        guard let city = cityField.text else {return}
+        guard let description = descriptionField.text else {return}
+
+        // Create new user instance
+        let newUser = User(id: username, firstName: firstName, lastName: lastName, password: password, city: city, description: description)
+        
+        // (Use dummy data for now) add user to database
+        let dummydataReader = JSONDummyDataReader()
+        dummydataReader.createUser(newUser: newUser)
+        
+        // Push Login Screen
+        let vc = storyboard?.instantiateViewController(identifier: "LoginViewController") as! LoginViewController
+        self.navigationController?.pushViewController(vc, animated: true)
     }
 }
 
