7936715178dd1cdbcf411368a2125fe38597a5d4
Toan Nguyen <toan@s1t2.com>
2023-05-22 12:18:42 +1000

    add constraints, add outlets

--- ios-ass3-promotion-network/Controller/SettingsViewController.swift
+++ ios-ass3-promotion-network/Controller/SettingsViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-
+    
+    @IBOutlet weak var profileContainerView: UIView!
+    
+    @IBOutlet weak var profilePictureField: UIImageView!
+    
+    @IBOutlet weak var logoutButton: UIButton!
+    @IBOutlet weak var aboutButton: UIButton!
+    @IBOutlet weak var locationField: UILabel!
+    @IBOutlet weak var nameField: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func aboutButtonHandler(_ sender: Any) {
+    }
+    
+    @IBAction func logoutButtonHandler(_ sender: Any) {
+    }
 }
 
