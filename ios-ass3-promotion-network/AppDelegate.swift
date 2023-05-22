50fe329d14e1e004fa757e041f1a1d7896680c18
malenadiazrio <106173028+malenadiazrio@users.noreply.github.com>
2023-05-23 00:34:58 +1000

    Deleted API AppDelegate.swift

--- ios-ass3-promotion-network/AppDelegate.swift
+++ ios-ass3-promotion-network/AppDelegate.swift
@@ -15,8 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        GMSServices.provideAPIKey("AIzaSyBqZ2uQbGP4hyVgV9bHudzdhqzqpFC-rZY")
-        GMSPlacesClient.provideAPIKey("AIzaSyBqZ2uQbGP4hyVgV9bHudzdhqzqpFC-rZY")
+        GMSServices.provideAPIKey("")
+        GMSPlacesClient.provideAPIKey("")
 
         // Override point for customization after application launch.
         return true
