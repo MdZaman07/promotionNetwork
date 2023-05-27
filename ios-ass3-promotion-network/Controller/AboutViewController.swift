1d4093d53ac874c249e130c3d54db59dbce9bd7a
Toan Nguyen <toan@s1t2.com>
2023-05-28 00:16:19 +1000

    link settings view, finish settings page functionalities, fix navigation bar for view profile/about/settings views

--- ios-ass3-promotion-network/Controller/AboutViewController.swift
+++ ios-ass3-promotion-network/Controller/AboutViewController.swift
@@ -14,6 +14,27 @@ class AboutViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    // Set navigation bar
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if let navigationController = self.navigationController {
+            var controllers = navigationController.viewControllers
+            
+            controllers = controllers.filter { controller in
+                return !(controller is LoginViewController)
+            }
+            
+            navigationController.topViewController?.navigationItem.title = "About"
+            navigationController.topViewController?.navigationItem.hidesBackButton = false
+            navigationController.viewControllers = controllers
+            navigationController.setNavigationBarHidden(false, animated: animated)
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
 
 }
 
