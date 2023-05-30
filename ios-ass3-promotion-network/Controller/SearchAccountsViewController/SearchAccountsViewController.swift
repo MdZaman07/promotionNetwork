1f60f576cc6f3ef87a93a1f06dae57ea90b5caa0
MD ZAMAN <pulokuzzaman2014@gmail.com>
2023-05-30 18:49:58 +1000

    added comments and fixed tableView

--- ios-ass3-promotion-network/Controller/SearchAccountsViewController/SearchAccountsViewController.swift
+++ ios-ass3-promotion-network/Controller/SearchAccountsViewController/SearchAccountsViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  ios-ass3-promotion-network
 //
-//  Created by Malena Diaz Rio on 2/5/23.
+//  Created by Pulok Uz Zaman on 2/5/23.
 //
 
 import UIKit
@@ -10,11 +10,11 @@ import UIKit
 class SearchAccountsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var userAccountsTableView: UITableView!
-    
     var loginSession: LoginSession?
     var userAccounts: [AppUser] = []
     var userNames: [String] = []
     var realmManager = RealmManager.shared
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // If there is no login session, push to login screen
@@ -25,27 +25,27 @@ class SearchAccountsViewController: UIViewController, UITableViewDataSource, UIT
             self.navigationController?.pushViewController(vc, animated: true)
         }
         guard let realm = realmManager.realm else { return }
+        //get all the users from the database
         userAccounts = Array(realm.objects(AppUser.self))
-        for userProfile in userAccounts{
+        for userProfile in userAccounts{ //add all the users' username tp the userNames variable
             userNames.append(userProfile.userName)
         }
         userAccountsTableView.dataSource = self
         userAccountsTableView.delegate = self
     }
     
-    
-    @IBAction func handleSearch(_ sender: UITextField) {
+    @IBAction func handleSearch(_ sender: UITextField) { //handles search everytime the textField is edited
+        guard let realm = realmManager.realm else { return }
         if let searchedText = sender.text{
-            guard !searchedText.isEmpty else{
-                userAccounts = Array(realmManager.realm!.objects(AppUser.self))
+            guard !searchedText.isEmpty else{ //if searchedTet is empty, assign all the users in the database to userAccounts
+                userAccounts = Array(realm.objects(AppUser.self))
                 userAccountsTableView.reloadData()
                 return
             }
-
-            userAccounts = Array(realmManager.realm!.objects(AppUser.self).filter { $0.userName.lowercased().contains(searchedText.lowercased()) })
+            //search filter is case insensitive
+            userAccounts = Array(realm.objects(AppUser.self).filter { $0.userName.lowercased().contains(searchedText.lowercased()) })
         }
-        userAccountsTableView.reloadData()
-                                                                  
+        userAccountsTableView.reloadData() //reload tableView with the matched userAccounts
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -73,22 +73,21 @@ class SearchAccountsViewController: UIViewController, UITableViewDataSource, UIT
         } else {
             tableViewCell.profileImageView.image = UIImage(systemName: "person.fill")
         }
-        
         return tableViewCell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
-        performSegue(withIdentifier: "viewProfileSegue", sender: userAccounts[indexPath.row])
+        performSegue(withIdentifier: "viewProfileSegue", sender: userAccounts[indexPath.row]) // show the profile of the selected user
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if (segue.identifier == "viewProfileSegue") {
             let userAccount = sender as! AppUser
             let viewProfile = segue.destination as! ViewProfileViewController
+            //assign the selected userAccount to the userProfile variable in viewProfile
             viewProfile.userProfile = userAccount
         }
     }
-
 
 }
 
